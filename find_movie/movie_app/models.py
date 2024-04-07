e66c9076c62b1133bfb3efbad1f918f27c1f7c63
Illia <doku_isv@icloud.com>
2024-04-07 18:11:23 +0200

    feat(DFM-2): Create new model for database
    Created new Movie model for database for future use in functions and features of project
    07/04/2024

--- find_movie/movie_app/models.py
+++ find_movie/movie_app/models.py
@@ -1,3 +1,19 @@
+"""Import necessary libraries."""
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """Movie model."""
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, help_text="The title of the movie.")
+    release_date = models.DateField(help_text="The date the movie was released.")
+    genre = models.CharField(max_length=255, help_text="The genre of the movie.")
+    movie_id = models.CharField(max_length=255, unique=True, help_text="Enter the id of the movie.")
+    imdb_id = models.CharField(max_length=255, unique=True, help_text="Enter the IMDb ID.")
+    plot = models.TextField(blank=True, help_text="The plot of the movie.")
+    response = models.CharField(max_length=10, choices=[("JSON", "JSON"), ("XML", "XML")],
+                                default="JSON", blank=True)
+
+    def __str__(self):
+        """String for representing the Movie object."""
+        return f"{self.title} ({self.release_date}) - {self.genre}"
