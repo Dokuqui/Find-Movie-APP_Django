5540c19a2d6f3bf3e1558c3a3a7f3f2e0ba9275d
Illia <doku_isv@icloud.com>
2024-05-01 16:21:44 +0200

    feat(DFM-3): Create init views func

--- find_movie/movie_app/views.py
+++ find_movie/movie_app/views.py
@@ -1,5 +1,31 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Movie
+import requests
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    """View function for home page"""
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        movie = Movie.objects.filter(title__icontains=title).first()
+        if movie:
+            return render(request, 'movies/movie_detail.html', {'movie': movie})
+        else:
+            api_url = 'http://www.omdbapi.com/?apikey=8dcbd9c7&t=' + title
+            response = requests.get(api_url)
+            data = response.json()
+            if data['Response'] == 'True':
+                movie = Movie.objects.create(
+                    title=data['Title'],
+                    release_date=data['Year'],
+                    genre=data['Genre'],
+                    movie_id=data['imdbID'],
+                    imdb_id=data['imdbID'],
+                    plot=data['Plot'],
+                    response=data['Type']
+                )
+                return render(request, 'movies/movie_detail.html', {'movie': movie})
+            else:
+                return render(request, 'movies/error.html', {'message': 'No results found.'})
+    else:
+        return render(request, 'movies/home.html')
