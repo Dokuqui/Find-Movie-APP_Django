cf2887b76b9fab7bba107c07bac171b002c8571d
Illia <doku_isv@icloud.com>
2024-05-01 23:03:57 +0200

    test(DFM-6): Add new test cases

--- find_movie/movie_app/tests.py
+++ find_movie/movie_app/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+"""Import necessary libraries."""
+from django.test import TestCase, RequestFactory
+from .views import movie_app
+from .models import Movie
+from faker import Faker
 
-# Create your tests here.
+
+class MovieAppViewTests(TestCase):
+    """Tests for the Movie app views"""
+
+    def setUp(self):
+        """Set up test fixtures"""
+        self.factory = RequestFactory()
+        self.fake = Faker()
+        self.test_movie = Movie.objects.create(
+            id=1,
+            title=self.fake.name(),
+            release_date=self.fake.date(),
+            runtime=self.fake.time(),
+            genre=self.fake.words(nb=3),
+            director=self.fake.name(),
+            actors=self.fake.name(),
+            movie_id=self.fake.uuid4(),
+            imdb_id=self.fake.uuid4(),
+            plot=self.fake.sentence(),
+            poster_url=self.fake.url()
+        )
+
+    def test_movie_detail_by_id(self):
+        """Test detail view by movie ID"""
+        request = self.factory.get('/movie_app/movie/', {'id': self.test_movie.pk})
+        response = movie_app(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.test_movie.id)
+
+    def test_movie_search_by_title(self):
+        """Test search view by movie title"""
+        request = self.factory.post('/movie_app/movie/', {'title': 'Famous Nonexistent Movie Quotes (PART 1)'})
+        response = movie_app(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Famous Nonexistent Movie Quotes (PART 1)')
+
+    def test_movie_search_no_results(self):
+        """Test search view with no results"""
+        request = self.factory.post('/movie_app/movie/', {'title': 'AESDSQZE'})
+        response = movie_app(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,  'Movie not found.')
+
+    def test_empty_search_term(self):
+        """Test search view with empty search term"""
+        request = self.factory.post('/movie_app/movie/', {'title': ''})
+        response = movie_app(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Search term cannot be empty.')
--- find_movie/movie_app/views.py
+++ find_movie/movie_app/views.py
@@ -14,53 +14,56 @@ load_dotenv()
 def movie_app(request):
     if request.method == 'POST':
         search_term = request.POST.get('title')
-        # Check if the search term is a valid movie ID
-        if search_term.isdigit():
-            try:
-                movie = Movie.objects.get(pk=int(search_term))
-                return render(request, 'movie_app/movie_detail.html', {'movie': movie})
-            except ObjectDoesNotExist:
-                return render(request, 'movie_app/error.html', {'message': 'Movie not found.'})
-        else:
-            # Search by title using OMDB API
-            OMDB_API_KEY = os.getenv('OMDB_API_KEY')
-            api_url = 'http://www.omdbapi.com/?t=' + search_term + f'&apikey={OMDB_API_KEY}'
-            response = requests.get(api_url)
-            data = response.json()
-
-            if data.get('Response') == 'True':
-                # Parse the release_date into a valid date format
-                release_year = ''.join(filter(str.isdigit, data['Year']))
+        if search_term:
+            # Check if the search term is a valid movie ID
+            if search_term.isdigit():
                 try:
-                    release_date = datetime.strptime(release_year, '%Y').date()
-                except ValueError:
-                    release_date = None
-
-                # Check if the movie already exists in the database
-                movie, created = Movie.objects.get_or_create(
-                    title=data['Title'],
-                    defaults={
-                        'release_date': release_date,
-                        'runtime': data['Runtime'],
-                        'genre': data['Genre'],
-                        'director': data['Director'],
-                        'actors': data['Actors'],
-                        'movie_id': data['imdbID'],
-                        'imdb_id': data['imdbID'],
-                        'plot': data['Plot'],
-                        'response': data['Type']
-                    }
-                )
-
-                if created:
-                    # Save the poster URL if it exists
-                    poster_url = data.get('Poster')
-                    if poster_url:
-                        movie.poster_url = poster_url
-                        movie.save()
-
-                return render(request, 'movie_app/movie_detail.html', {'movie': movie})
+                    movie = Movie.objects.get(pk=int(search_term))
+                    return render(request, 'movie_app/movie_detail.html', {'movie': movie})
+                except ObjectDoesNotExist:
+                    return render(request, 'movie_app/error.html', {'message': 'Movie not found.'})
             else:
-                return render(request, 'movie_app/error.html', {'message': 'No results found.'})
+                # Search by title using OMDB API
+                OMDB_API_KEY = os.getenv('OMDB_API_KEY')
+                api_url = 'http://www.omdbapi.com/?t=' + search_term + f'&apikey={OMDB_API_KEY}'
+                response = requests.get(api_url)
+                data = response.json()
+
+                if data.get('Response') == 'True':
+                    # Parse the release_date into a valid date format
+                    release_year = ''.join(filter(str.isdigit, data['Year']))
+                    try:
+                        release_date = datetime.strptime(release_year, '%Y').date()
+                    except ValueError:
+                        release_date = None
+
+                    # Check if the movie already exists in the database
+                    movie, created = Movie.objects.get_or_create(
+                        title=data['Title'],
+                        defaults={
+                            'release_date': release_date,
+                            'runtime': data['Runtime'],
+                            'genre': data['Genre'],
+                            'director': data['Director'],
+                            'actors': data['Actors'],
+                            'movie_id': data['imdbID'],
+                            'imdb_id': data['imdbID'],
+                            'plot': data['Plot'],
+                            'response': data['Type']
+                        }
+                    )
+
+                    if created:
+                        # Save the poster URL if it exists
+                        poster_url = data.get('Poster')
+                        if poster_url:
+                            movie.poster_url = poster_url
+                            movie.save()
+
+                    return render(request, 'movie_app/movie_detail.html', {'movie': movie})
+                else:
+                    return render(request, 'movie_app/error.html', {'message': 'Movie not found.'})
+        else:
+            return render(request, 'movie_app/error.html', {'message': 'Search term cannot be empty.'})
     else:
         return render(request, 'movie_app/home.html')
