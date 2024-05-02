43726428cb1605e03d221d1020552b3003ced136
Illia <doku_isv@icloud.com>
2024-05-02 17:44:13 +0200

    feat(DFM-13): Add footer and nav

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("movie/", views.movie_app, name='movie_app'),
+    path("movie/", views.movie_app, name='home'),
+    path("movie/usage", views.movie_app, name='usage'),
+    path("movie/contact", views.movie_app, name='contact')
 ]
