23e939c7ac90aa40e216841676efd4225c220729
Illia <136450403+Dokuqui@users.noreply.github.com>
2024-05-06 08:28:22 +0000

    refactor(DFM-16): update project url and req

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("movie/", views.movie_app, name='home'),
-    path("movie/usage", views.usage, name='usage'),
-    path("movie/contact", views.contact, name='contact')
+    path("", views.movie_app, name='home'),
+    path("/usage", views.usage, name='usage'),
+    path("/contact", views.contact, name='contact')
 ]
